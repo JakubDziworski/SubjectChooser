fe45d0c583d302682adf1cfa45119302acc4a13f
jakubdziworski <jakub.dziworski@gmail.com>
2015-04-22 19:56:04 +0200

    -UI

---
+++ src/application/ApplicationManager.java
@@ -0,0 +1,7 @@
+package application;
+
+public class ApplicationManager {
+	public class Resources {
+		public static final String VIEW_MAIN_WINDOW_FILE_PATH = "/view/MainWindowGUIProject.fxml";
+	}
+}
---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			final String mainWindowFilePath = ApplicationManager.Resources.VIEW_MAIN_WINDOW_FILE_PATH;
+			final URL mainWindowFileURL = getClass().getResource(mainWindowFilePath);
+			Parent root = FXMLLoader.load(mainWindowFileURL);
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
